a775bd3f8c188d69d707cf831c0d785e214582e1
Alex Brea <bgalejandro97@gmail.com>
2021-09-28 21:52:38 +0200

    CSS-JS-HTML: Added modal, search by click in character card, styles and fix some responsive issues.

---
+++ README.md
@@ -0,0 +1,18 @@
+<h1 align="center">Rick And Morty API Test Site</h1>
+
+## About ##
+
+Welcome to this simple project where, using the Rick & Morty API, I show all the characters, with a button in charge of loading more characters and the functionality of, when clicking on a character, loading a modal with all its information.
+
+This project has been created in order to better understand how it work.
+
+You can copy, distribute and modify the content of this repository if you wish, as long as it is for educational purposes and not for sale or distribution.
+
+##Technologies ##
+
+The following tools were used in this project:
+
+- HTML.
+- CSS.
+- Javascript.
+- Bootstrap 5.
--- assets/js/main.js
+++ assets/js/main.js
@@ -11,6 +11,13 @@ const API_GET_CHARACTER_BY_NAME = `${API_CHARACTERS_URL}?name=`;
 /* DOM */
 let cards_container = document.querySelector('.js-card-container');
 let load_more_btn = document.querySelector('.js-load-more-btn');
+/*  Modal */
+const modal_character_image = document.querySelector('.js-modal-character-image');
+const modal_character_name = document.querySelector('.js-modal-character-name');
+const modal_character_species = document.querySelector('.js-modal-character-species');
+const modal_character_gender = document.querySelector('.js-modal-character-gender');
+const modal_character_origin = document.querySelector('.js-modal-character-origin');
+const modal_character_location = document.querySelector('.js-modal-character-location');
 
 /* === END Const and vars === */
 
@@ -44,7 +51,7 @@ function generateCards( cards ) {
 
     let card_info = `
       <div class="col-12 col-md-6 col-lg-4 col-xl-3 mb-4">
-        <div class="c-card">
+        <div onclick="getCharacterInformation(${card.id})" class="c-card" data-bs-toggle="modal" data-bs-target="#characterInformationModal">
           <div class="c-card__head">
             <img src="${card.image}">
           </div>
@@ -86,6 +93,22 @@ function removeAllChildNodes(parent) {
 /* Add data to root container */
 function writeText(text) {
   root.innerHTML += `<p>${text}</p>`;
+}
+
+/* Get data from character by ID and modify modal before showing */
+function getCharacterInformation(character_id) {
+  
+  fetch(API_CHARACTERS_URL + character_id)
+    .then(response => response.json())
+    .then(data => {
+      modal_character_image.setAttribute( 'src', data.image )
+      modal_character_name.innerHTML = data.name;
+      modal_character_species.innerHTML = `Specie: ${data.species}`;
+      modal_character_gender.innerHTML = `Gender: ${data.gender}`;
+      modal_character_origin.innerHTML = `Origin: ${data['origin'].name}`;
+      modal_character_location.innerHTML = `Current location: ${data['location'].name}`;
+    });
+
 }
 
 /* Init get data */
